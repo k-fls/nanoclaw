5f51a96a06302d81f5627311067ad37361ec8a96
Cyrax <12501906+cyraxred@users.noreply.github.com>
2026-04-11 21:02:40 +0400

    Fix send

--- src/commands/auth-commands.ts
+++ src/commands/auth-commands.ts
@@ -218,7 +218,7 @@ registerCommand('auth-gpg', {
     ensureGpgKey(groupScope);
     return {
       asyncAction: async (io) => {
-        await io.send(exportPublicKey(groupScope));
+        await io.sendRaw(exportPublicKey(groupScope));
       },
     };
   },
